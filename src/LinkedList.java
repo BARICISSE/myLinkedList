686c3f6817d9af9f6af98593c1d08e5324fc83da
AnssamGhezala <anssam.ghezala@mail.mcgill.ca>
2018-11-09 23:08:11 -0500

    populate LinkedList class + addFirst() method

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,32 @@
+import java.util.*;
 
 public class LinkedList {
-  
+	public Node head;
+	
+
+/*
+ * addFirst
+ * addLast
+ * addAt(e,newNode)
+ *
+ */
+	
+	public void addFirst(Node newNode) {
+	//	System.out.println(" head " + head);
+		 newNode.next = head;
+		head = newNode;
+		
+	}
+	
+	public String toString(){
+		 ArrayList<Integer> list = new ArrayList<>();
+		 list.add(head.data);
+		Node temp = head;
+		 while(temp.next != null) {
+			 list.add(temp.next.data);
+			 temp = temp.next;
+		 }
+		 
+		  return list.toString();
+	  }
 }
--- src/LinkedListImplementation.java
+++ src/LinkedListImplementation.java
@@ -1,14 +1,28 @@
 
 public class LinkedListImplementation {
-
+	
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
     Node n = new Node();
     n.data = 6;
     Node newNode = new Node();
     newNode.data = 7;
     n.next = newNode;
-    System.out.println(n.toString());
+   // System.out.println(n.toString());
+    
+    
+    LinkedList myList = new LinkedList();
+    
+    myList.head = n;
+    
+    System.out.println(" old list " + myList.toString());
+    
+    
+    
+    Node newNode1 = new Node();
+    newNode1.data = 5;
+   myList.addFirst(newNode1);
+   System.out.println(" new list " +  myList.toString());
+    
   }
 
 }
